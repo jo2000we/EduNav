76743b903f34ac3feb0f55ea289fa9c6fbf0cabd
jo2000we <134441951+jo2000we@users.noreply.github.com>
2025-08-14 17:30:42 +0200

    Prevent duplicate reflections

--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
+from django.db import IntegrityError
 
 from accounts.models import User
 from lessons.models import Classroom, LessonSession, UserSession
 from goals.models import Goal
+from reflections.models import Reflection
 
 
 class ReflectionAiToggleTests(TestCase):
@@ -43,3 +45,29 @@ class ReflectionEdgeCaseTests(TestCase):
         )
         self.assertEqual(resp.status_code, 400)
         self.assertIn("result", resp.json())
+
+
+class ReflectionUniquenessTests(TestCase):
+    def setUp(self):
+        self.classroom = Classroom.objects.create(name="10A")
+        self.user = User.objects.create_user(pseudonym="u1", classroom=self.classroom)
+        lesson = LessonSession.objects.create(date=timezone.now().date(), classroom=self.classroom)
+        self.session = UserSession.objects.create(user=self.user, lesson_session=lesson)
+        self.goal = Goal.objects.create(user_session=self.session, raw_text="test")
+
+    def test_duplicate_reflection_not_allowed(self):
+        Reflection.objects.create(
+            user_session=self.session,
+            goal=self.goal,
+            result="yes",
+            obstacles="keine",
+            next_step="weiter",
+        )
+        with self.assertRaises(IntegrityError):
+            Reflection.objects.create(
+                user_session=self.session,
+                goal=self.goal,
+                result="no",
+                obstacles="keine",
+                next_step="weiter",
+            )
