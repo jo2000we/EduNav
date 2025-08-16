518f65544425037ad98bf1ba483c52b536e6633c
jo2000we <134441951+jo2000we@users.noreply.github.com>
2025-08-16 05:51:15 +0200

    Register models in Django admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Classroom, Student, LearningGoal
+
+
+class StudentInline(admin.TabularInline):
+    model = Student
+    extra = 1
+
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ["name", "teacher", "group_type"]
+    list_filter = ["teacher", "group_type"]
+    inlines = [StudentInline]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["pseudonym", "classroom"]
+    list_filter = ["classroom"]
+
+
+@admin.register(LearningGoal)
+class LearningGoalAdmin(admin.ModelAdmin):
+    list_display = ["student", "session_date", "achieved"]
+    list_filter = ["achieved", "session_date"]
